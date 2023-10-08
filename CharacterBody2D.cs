6063631e6dadf2dac10dd0528dc9cab94d0f8233
abildgaardo <thba@eucnord.dk>
2023-10-08 20:39:51 +0200

    Update CharacterBody2D.cs

--- CharacterBody2D.cs
+++ CharacterBody2D.cs
@@ -3,7 +3,7 @@ using System;
 
 public partial class CharacterBody2D : Godot.CharacterBody2D
 {
-	public const float Speed = 300.0f;
+	public const float Speed = 200.0f;
 	public const float JumpVelocity = -400.0f;
 
 	// Get the gravity from the project settings to be synced with RigidBody nodes.
